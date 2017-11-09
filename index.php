a9f181c6e9dd325f5a4c145d77dac31ae31c9359
Nikolay Andrievskiy <gazab@yandex.ru>
2017-11-09 23:43:17 +0300

    +

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GazaB</title>
+</head>
+<body>
+    <h1>Gazab</h1>
+</body>
+</html>
